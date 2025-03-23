9f3c9076701d5c434e512ac77e9dc14c5b64b045
Chamodhie Gamage <71579829+chamo111@users.noreply.github.com>
2025-03-23 18:14:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,25 @@ with st.expander('Data Visualization'):
 with st.sidebar:
   st.header('Input Features')
   island = st.selectbox('Island', ('Biscoe','Dream','Torgersen'))
-  gender = st.selectbox('Gender',('male', 'female'))
   bill_length_mm = st.slider('Bill Langth (mm)',32.1,59.6,43.9) #st.slider(label, min_value=None, max_value=None, step=None,
   bill_depth_mm = st.slider('Bill Depth (mm)', 13.1,21.5,17.2)
   flipper_length_mm = st.slider('Flipper Length (mm)', 172.0,231.0,201.0)
   body_mass_g = st.slider('Body Mass(g)', 2700.0, 6300.0, 4207.0)
+  gender = st.selectbox('Gender',('male', 'female'))
+
+#create a date frame for the input features
+  daa = {'island': island,
+       'bill_length_mm': bill_length_mm,
+       'bill_depth_mm': bill_depth_mm,
+       'flipper_length_mm':flipper_length_mm,
+       'body_mass_g':body_mass_g,
+       'gender': gender}
+  input_df = pd.DataFrame(data,index=[0])
+  input_penguins = pd.concat([input_df,X], axis = 0) #combines two dataframes
+
+input_penguins
+
+
+
+
+
