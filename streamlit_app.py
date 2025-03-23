ee18fa385965b8e7c053eb41b062d8694031165c
Chamodhie Gamage <71579829+chamo111@users.noreply.github.com>
2025-03-23 16:47:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title('Machine Learning App')
 
 st.info('This app builds a Machine Learning Model!')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+df
