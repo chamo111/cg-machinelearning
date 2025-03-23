9c82d29752bf66fa40eb6faba60f67901904d28c
Chamodhie Gamage <71579829+chamo111@users.noreply.github.com>
2025-03-23 22:33:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,44 +83,44 @@ with st.expander('Data Preparation'):
 clf = RandomForestClassifier()
 clf.fit(X, y) 
 
-### Apply model to make predictions
-#prediction = clf.predict(input_row)
-#prediction_proba = clf.predict_proba(input_row)
+## Apply model to make predictions
+prediction = clf.predict(input_row)
+prediction_proba = clf.predict_proba(input_row)
 
-#df_prediction_proba = pd.DataFrame(prediction_proba)
-#df_prediction_proba.columns = ['Adelie','Chinstrap','Gentoo']
+df_prediction_proba = pd.DataFrame(prediction_proba)
+df_prediction_proba.columns = ['Adelie','Chinstrap','Gentoo']
 
 
-##Display Predicted species
-#st.subheader('Predicted Species')
-#st.dataframe(df_prediction_proba,
- #            column_config = {
-  #             'Adelie': st.column_config.ProgressColumn(
-   #              'Adelie', 
-    #             format='%f',
-     #            width='medium',
-      #           min_value=0,
-       #          max_value=1
-        #       ),
- #              'Chinstrap': st.column_config.ProgressColumn(
-  #               'Chinstrap', 
-   #              format='%f',
-    #             width='medium',
-     #            min_value=0,
-      #           max_value=1
-       #        ),
-#               'Gentoo': st.column_config.ProgressColumn(
- #                'Gentoo', 
-  #               format='%f',
-   #              width='medium',
-    #             min_value=0,
-     #            max_value=1
-      #         )
-       #      }, hide_index=True)
-#df_prediction_proba
+#Display Predicted species
+st.subheader('Predicted Species')
+st.dataframe(df_prediction_proba,
+             column_config = {
+               'Adelie': st.column_config.ProgressColumn(
+                 'Adelie', 
+                 format='%f',
+                 width='medium',
+                 min_value=0,
+                 max_value=1
+               ),
+               'Chinstrap': st.column_config.ProgressColumn(
+                 'Chinstrap', 
+                 format='%f',
+                 width='medium',
+                 min_value=0,
+                 max_value=1
+               ),
+               'Gentoo': st.column_config.ProgressColumn(
+                 'Gentoo', 
+                 format='%f',
+                 width='medium',
+                 min_value=0,
+                 max_value=1
+               )
+             }, hide_index=True)
+df_prediction_proba
 
-#penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
-#st.success(str(penguins_species[prediction][0]))
+penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
+st.success(str(penguins_species[prediction][0]))
 
 
 
